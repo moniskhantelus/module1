4e9efcede6b2ad4258b78cf43a983d9cee102edd
moniskhantelus <93679325+moniskhantelus@users.noreply.github.com>
2024-06-11 01:10:46 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,3 @@
-/**
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 output "instances_self_links" {
   description = "List of self-links for compute instances"
@@ -28,4 +13,23 @@ output "instances_details" {
 output "available_zones" {
   description = "List of available zones in region"
   value       = data.google_compute_zones.available.names
+}
+output "self_link_unique" {
+  description = "Unique self-link of instance template (recommended output to use instead of self_link)"
+  value       = google_compute_instance_template.tpl.self_link_unique
+}
+
+output "self_link" {
+  description = "Self-link of instance template"
+  value       = google_compute_instance_template.tpl.self_link
+}
+
+output "name" {
+  description = "Name of instance template"
+  value       = google_compute_instance_template.tpl.name
+}
+
+output "tags" {
+  description = "Tags that will be associated with instance(s)"
+  value       = google_compute_instance_template.tpl.tags
 }
